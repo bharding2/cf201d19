12992b075af8941a5143a214c8e42bcc67153f1f
bharding2 <b.harding@gmail.com>
2017-02-21 12:21:01 -0800

    charting

--- inclass/tue_2-21/app.js
+++ inclass/tue_2-21/app.js
@@ -1,1 +1,33 @@
 'use strict';
+
+var ctx = document.getElementById('chart').getContext('2d');
+
+var data = [12, 4, 9, 3, 100, 55, 31];
+var labelColor = ['blue', 'red', 'orange', 'purple', 'green', 'yellow', 'salmon'];
+
+var chartData = {
+  type: 'bar',
+  data: {
+    labels: labelColor,
+    datasets: [
+      {
+        label: '# of Votes / Color',
+        data: data,
+        backgroundColor: labelColor
+      }
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+};
+
+var myChart = new Chart(ctx, chartData);
